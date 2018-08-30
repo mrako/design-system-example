6509283450d4d5976a419d0b7e8b840af7c56282
Marko Klemetti <marko.klemetti@gmail.com>
2018-08-30 09:16:51 +0300

    Fix lint

--- frontend/src/containers/home/HomePage.jsx
+++ frontend/src/containers/home/HomePage.jsx
@@ -1,17 +1,13 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 
 import Footer from '../../components/Footer';
 
-class HomePage extends Component {
-  render() {
-    return (
-      <Fragment>
-        <h1 className="uk-text-center">Welcome!</h1>
+const HomePage = () => (
+  <Fragment>
+    <h1 className="uk-text-center">Welcome!</h1>
 
-        <Footer />
-      </Fragment>
-    );
-  }
-}
+    <Footer />
+  </Fragment>
+);
 
 export default HomePage;
