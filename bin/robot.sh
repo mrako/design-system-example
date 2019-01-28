68f5b710663978fd0808d3ddd0dca4486d795c68
Marko Klemetti <marko.klemetti@gmail.com>
2019-01-28 09:57:32 +0100

    Add down after robot tests

--- bin/robot.sh
+++ bin/robot.sh
@@ -3,5 +3,8 @@
 set -e
 
 docker-compose --project-directory . -f docker-compose.yml -f compose/test.yml -f compose/robot.yml down -v
+
 docker-compose --project-directory . -f compose/robot.yml run robot.backend ./wait-for robot.db:5432 -- npm run db:seed:all
 docker-compose --project-directory . -f compose/robot.yml run robot
+
+docker-compose --project-directory . -f compose/robot.yml down
